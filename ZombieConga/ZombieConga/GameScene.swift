d012cb20b639dac79e39cfde54c08ccb73ab926e
Cody Morley <codymorleyinc@gmail.com>
2020-09-02 20:55:56 -0700

    Animation - Zombie Sprite
    Zombie Sprite movement on x-axis

--- ZombieConga/ZombieConga/GameScene.swift
+++ ZombieConga/ZombieConga/GameScene.swift
@@ -10,23 +10,29 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    // Sprites
+    let background = SKSpriteNode(imageNamed: "background1") ///This is a sprite it uses SKSpriteNode
+    let zombie = SKSpriteNode(imageNamed: "zombie1")
+    
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black ///spritekit uses colors of type SKColor
-        // Sprites
-        let background = SKSpriteNode(imageNamed: "background1") ///This is a sprite it uses SKSpriteNode
-        let zombie = SKSpriteNode(imageNamed: "zombie1")
         
         background.anchorPoint = CGPoint(x: 0.5, y: 0.5) /// default
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         background.zPosition = -1
         //background.zRotation = CGFloat.pi / 8
-        zombie.position = CGPoint(x: 400, y: 400)
-        zombie.scale(to: CGSize(width: zombie.size.width * 2, height: zombie.size.height * 2))
         
+        zombie.position = CGPoint(x: 400, y: 400)
+        //zombie.scale(to: CGSize(width: zombie.size.width * 2, height: zombie.size.height * 2)) ///Method of SKSpriteNode
+        //zombie.setScale(2) ///Method of SKNode
         
         addChild(background) /// This adds the sprite (SKNode) as a child of the scene (SKScene)
         addChild(zombie)
-        let mySize = background.size
-        print("Size: \(mySize)")
+        //let mySize = background.size
+        //print("Size: \(mySize)")
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        zombie.position = CGPoint(x: zombie.position.x + 8, y: zombie.position.y)
     }
 }
